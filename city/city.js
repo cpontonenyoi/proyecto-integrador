2deb20d081f2d41a1ab7f3a55e738007563cd3e3
Carlos Ponton <carlos.ponton@enyoi.co>
2023-09-27 19:49:43 -0500

    comment get all city

--- city/city.js
+++ city/city.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express.Router();
 const City = require('./city.model');
 
+/**
+ * The endpoint list all city
+ */
 app.get('/city', async function (req, res) {
     let city = await City.findAll();
     res.send(city);
